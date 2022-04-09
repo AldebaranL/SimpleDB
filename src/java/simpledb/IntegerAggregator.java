13b16338c85135b23f56758ac6ab61809a868c13
AldebaranL <cynthia.y.lin@outlook.com>
2022-04-09 17:13:36 +0800

    finish lab2 exercise2

--- src/java/simpledb/IntegerAggregator.java
+++ src/java/simpledb/IntegerAggregator.java
@@ -1,5 +1,9 @@
 package simpledb;
 
+import java.util.*;
+
+import static simpledb.Aggregator.Op.AVG;
+
 /**
  * Knows how to compute some aggregate over a set of IntFields.
  */
@@ -7,6 +11,48 @@ public class IntegerAggregator implements Aggregator {
 
     private static final long serialVersionUID = 1L;
 
+    private int gbfieldNo;
+    private Type gbfieldType;
+    private Op aggregationOp;
+    private int afieldNo;
+    private List<Tuple> groups;//也可不建立此list，当需要iterate时根据group直接生成new Tuple，实现方式具见StringAggregator
+    private HashMap<Field,group> gfieldToGroup;//HashMap支持Key为null
+    private TupleDesc td;
+
+    class group{
+        private int  AVG_count;
+        private int aggregateVal;
+        private int index;//该group在groups中的index
+        private group(int index,int aggregateVal){
+            this.AVG_count=1;
+            this.aggregateVal=aggregateVal;
+            this.index=index;
+        }
+        private void update(int newAggregateVal){
+            this.AVG_count++;
+            switch (aggregationOp){
+                case AVG:
+                    this.aggregateVal+=newAggregateVal;
+                    break;
+                case MAX:
+                    this.aggregateVal=Math.max(this.aggregateVal,newAggregateVal);
+                    break;
+                case MIN:
+                    this.aggregateVal=Math.min(this.aggregateVal,newAggregateVal);
+                    break;
+                case COUNT:
+                    this.aggregateVal=this.AVG_count;
+                    break;
+                case SUM:
+                    this.aggregateVal+=newAggregateVal;
+                    break;
+            }
+        }
+        private int getValue(){
+            if(aggregationOp.equals(AVG)) return this.aggregateVal/this.AVG_count;
+            else return this.aggregateVal;
+        }
+    }
     /**
      * Aggregate constructor
      * 
@@ -24,6 +70,13 @@ public class IntegerAggregator implements Aggregator {
 
     public IntegerAggregator(int gbfield, Type gbfieldtype, int afield, Op what) {
         // some code goes here
+        this.afieldNo=afield;
+        this.aggregationOp=what;
+        this.gbfieldNo=gbfield;
+        this.gbfieldType=gbfieldtype;
+        this.groups=new ArrayList<>();
+        this.gfieldToGroup=new HashMap<>();
+        this.td=null;
     }
 
     /**
@@ -35,6 +88,41 @@ public class IntegerAggregator implements Aggregator {
      */
     public void mergeTupleIntoGroup(Tuple tup) {
         // some code goes here
+        //若首次merge需要创建TupleDesc
+        if(this.td==null){
+            Type[] typeAr;
+            String[] fieldAr;
+            if(gbfieldNo==Aggregator.NO_GROUPING) {
+                typeAr = new Type[]{Type.INT_TYPE};
+                fieldAr = new String[]{tup.getTupleDesc().getFieldName(this.afieldNo)};
+            }
+            else{
+                typeAr=new Type[]{this.gbfieldType,Type.INT_TYPE};
+                fieldAr=new String[]{tup.getTupleDesc().getFieldName(this.gbfieldNo),tup.getTupleDesc().getFieldName(this.afieldNo)};
+            }
+            this.td=new TupleDesc(typeAr,fieldAr);
+        }
+
+        Field gfield = tup.getField(this.gbfieldNo);//若gbfieldNo==Aggregator.NO_GROUPING,gfield=null
+        //添加或更新gfieldToGroup
+        if(!gfieldToGroup.containsKey(gfield))
+            gfieldToGroup.put(gfield,
+                    new group(gfieldToGroup.size(),((IntField)tup.getField(this.afieldNo)).getValue()));
+        else
+            gfieldToGroup.get(gfield).
+                    update(((IntField)tup.getField(this.afieldNo)).getValue());
+        //创建新Tuple
+        Tuple newTp = new Tuple(this.td);
+        if(gbfieldNo!=Aggregator.NO_GROUPING) {
+            newTp.setField(0,tup.getField(this.gbfieldNo));
+            newTp.setField(1,new IntField(gfieldToGroup.get(gfield).getValue()));
+        }
+        else{
+            newTp.setField(0,new IntField(gfieldToGroup.get(gfield).getValue()));
+        }
+        //创建或更新groups(List<Tuple>)
+        if(gfieldToGroup.get(gfield).index==groups.size()) groups.add(newTp);
+        else groups.set(gfieldToGroup.get(gfield).index,newTp);
     }
 
     /**
@@ -47,8 +135,44 @@ public class IntegerAggregator implements Aggregator {
      */
     public OpIterator iterator() {
         // some code goes here
-        throw new
-        UnsupportedOperationException("please implement me for lab2");
+        //直接通过groups遍历
+        final int[] nowIndex = new int[1];
+        return new OpIterator() {
+            @Override
+            public void open() throws DbException, TransactionAbortedException {
+                nowIndex[0] = 0;
+            }
+
+            @Override
+            public boolean hasNext() throws DbException, TransactionAbortedException {
+                return (nowIndex[0] < groups.size()) && (nowIndex[0] >= 0);
+            }
+
+            @Override
+            public Tuple next() throws DbException, TransactionAbortedException, NoSuchElementException {
+                if(!this.hasNext()) throw new NoSuchElementException();
+                return groups.get(nowIndex[0]++);
+                //return null;
+            }
+
+            @Override
+            public void rewind() throws DbException, TransactionAbortedException {
+                this.close();
+                this.open();
+            }
+
+            @Override
+            public TupleDesc getTupleDesc() {
+                return td;
+            }
+
+            @Override
+            public void close() {
+                nowIndex[0]=-1;
+            }
+        };
+        //throw new
+       // UnsupportedOperationException("please implement me for lab2");
     }
 
 }
--- src/java/simpledb/StringAggregator.java
+++ src/java/simpledb/StringAggregator.java
@@ -1,5 +1,7 @@
 package simpledb;
 
+import java.util.*;
+
 /**
  * Knows how to compute some aggregate over a set of StringFields.
  */
@@ -7,6 +9,26 @@ public class StringAggregator implements Aggregator {
 
     private static final long serialVersionUID = 1L;
 
+    private int gbfieldNo;
+    private Type gbfieldType;
+    private Op aggregationOp;
+    private int afieldNo;
+    private HashMap<Field, group> gfieldToGroup;
+    private TupleDesc td;
+
+    class group{
+        private int count;
+        private group(){
+            this.count=1;
+        }
+        private void update(Op aggregationOp){
+            if (aggregationOp == Op.COUNT) {
+                this.count++;
+            } else {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
     /**
      * Aggregate constructor
      * @param gbfield the 0-based index of the group-by field in the tuple, or NO_GROUPING if there is no grouping
@@ -18,6 +40,12 @@ public class StringAggregator implements Aggregator {
 
     public StringAggregator(int gbfield, Type gbfieldtype, int afield, Op what) {
         // some code goes here
+        this.afieldNo=afield;
+        this.aggregationOp=what;
+        this.gbfieldNo=gbfield;
+        this.gbfieldType=gbfieldtype;
+        this.gfieldToGroup=new HashMap<>();
+        this.td=null;
     }
 
     /**
@@ -26,6 +54,25 @@ public class StringAggregator implements Aggregator {
      */
     public void mergeTupleIntoGroup(Tuple tup) {
         // some code goes here
+        Field gfield = tup.getField(this.gbfieldNo);
+        //添加或更新gfieldToGroup
+        if(!gfieldToGroup.containsKey(tup.getField(this.gbfieldNo))) gfieldToGroup.put(gfield,new group());
+        else gfieldToGroup.get(gfield).update(this.aggregationOp);
+
+        //若首次merge需要创建TupleDesc
+        if(this.td==null){
+            Type[] typeAr;
+            String[] fieldAr;
+            if(gbfieldNo==Aggregator.NO_GROUPING) {
+                typeAr = new Type[]{Type.INT_TYPE};
+                fieldAr = new String[]{tup.getTupleDesc().getFieldName(this.afieldNo)};
+            }
+            else{
+                typeAr=new Type[]{this.gbfieldType,Type.INT_TYPE};
+                fieldAr=new String[]{tup.getTupleDesc().getFieldName(this.gbfieldNo),tup.getTupleDesc().getFieldName(this.afieldNo)};
+            }
+            this.td=new TupleDesc(typeAr,fieldAr);
+        }
     }
 
     /**
@@ -36,9 +83,55 @@ public class StringAggregator implements Aggregator {
      *   grouping. The aggregateVal is determined by the type of
      *   aggregate specified in the constructor.
      */
+
+    Iterator<Field> gbfieldIt;
     public OpIterator iterator() {
         // some code goes here
-        throw new UnsupportedOperationException("please implement me for lab2");
+        //通过gfieldToGroup的Key集合（Field）遍历全部group
+        return new OpIterator() {
+            @Override
+            public void open() throws DbException, TransactionAbortedException {
+                gbfieldIt = gfieldToGroup.keySet().iterator();
+            }
+
+            @Override
+            public boolean hasNext() throws DbException, TransactionAbortedException {
+                return gbfieldIt.hasNext();
+            }
+
+            @Override
+            public Tuple next() throws DbException, TransactionAbortedException, NoSuchElementException {
+                if(!this.hasNext()) throw new NoSuchElementException();
+                Field nextField = gbfieldIt.next();
+                //创建新Tuple
+                Tuple newTp = new Tuple(td);
+                if(gbfieldNo!=Aggregator.NO_GROUPING) {
+                    newTp.setField(0,nextField);
+                    newTp.setField(1,new IntField(gfieldToGroup.get(nextField).count));
+                }
+                else{
+                    newTp.setField(0,new IntField(gfieldToGroup.get(nextField).count));
+                }
+                return newTp;
+                //return null;
+            }
+
+            @Override
+            public void rewind() throws DbException, TransactionAbortedException {
+                this.close();
+                this.open();
+            }
+
+            @Override
+            public TupleDesc getTupleDesc() {
+                return td;
+            }
+
+            @Override
+            public void close() {
+                gbfieldIt=null;
+            }
+        };
     }
 
 }
